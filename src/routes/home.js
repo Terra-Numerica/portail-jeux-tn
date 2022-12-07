566a66c8d8a2b1c5c4ebee87bbfb21959a8ecbf3
KushiieDev <riviere.lisa1875@gmail.com>
2022-12-07 14:37:49 +0100

    <feat> update home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 const router = Router();
 
-router.get('/', (req, res) => res.render('home/accueil.ejs'));
+router.get('/', (req, res) => res.render('home/accueil'));
+
+router.get('/liste', (req, res) => res.render('home/liste'));
+
+router.get('/ressources', (req, res) => res.render('home/ressources'))
 
 export default router;
