62ebab5deafa4fafcc5aa88ee6d44ad58a082eb0
antoine-agre <antoine.agre@free.fr>
2022-12-14 10:46:25 +0100

    <feat> add prototype liste page

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,9 +1,10 @@
+import jeux from '../public/json/jeux.json' assert {type: 'json'};
 import { Router } from "express";
 const router = Router();
 
 router.get('/', (req, res) => res.render('home/accueil'));
 
-router.get('/liste', (req, res) => res.render('home/liste'));
+router.get('/liste', (req, res) => res.render('home/liste', {jeux : jeux}));
 
 router.get('/ressources', (req, res) => res.render('home/ressources'))
 
