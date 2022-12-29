543dc3c04608f10f3b2346df20cf9347418a745a
KushiieDev <riviere.lisa1875@gmail.com>
2022-12-29 15:20:04 +0100

    <fix> fix bug on the footer but not optimized

--- src/public/js/footer.js
+++ src/public/js/footer.js
@@ -2,6 +2,11 @@ const footer = document.querySelector("footer");
 
 if(!hasOverflow(document.body)) footer.style.bottom = 0;
 
+window.addEventListener("resize", () => {
+    if(!hasOverflow(document.body)) footer.style.bottom = 0;
+    else footer.style.bottom = null;
+}, false);
+
 function hasOverflow(element){
     return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
 };
