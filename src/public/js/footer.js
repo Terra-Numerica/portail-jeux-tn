75207551e3bec78068edc49d0eaca1d5efc3481c
KushiieDev <riviere.lisa1875@gmail.com>
2023-01-02 12:07:45 +0100

    <feat> add optimized but not the best

--- src/public/js/footer.js
+++ src/public/js/footer.js
@@ -3,6 +3,9 @@ const footer = document.querySelector("footer");
 if(!hasOverflow(document.body)) footer.style.bottom = 0;
 
 window.addEventListener("resize", () => {
+
+    if(!hasOverflow(document.body) && footer.style.bottom === "0px") return;
+
     if(!hasOverflow(document.body)) footer.style.bottom = 0;
     else footer.style.bottom = null;
 }, false);
